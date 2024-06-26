c7171f999808cfdf45dd59e1d3e9bac5b1388de0
zkkv <zkkv@proton.me>
2024-06-26 22:31:37 +0200

    Configure footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 export default function Footer() {
 	return (
 		<footer>
-			<menu>
-				<li><a href="https://twitter.com">Twitter</a></li>
-				<li><a href="https://facebook.com">Facebook</a></li>
-				<li><a href="https://instagram.com">Instagram</a></li>
-				<li><a href="https://github.com">GitHub</a></li>
+			<menu id="footer-menu">
+				<li><a id="twitter-anchor" href="https://twitter.com">Twitter</a></li>
+				<li><a id="facebook-anchor" href="https://facebook.com">Facebook</a></li>
+				<li><a id="instagram-anchor" href="https://instagram.com">Instagram</a></li>
+				<li><a id="github-anchor" href="https://github.com">GitHub</a></li>
 			</menu>
 		</footer>
 	)
