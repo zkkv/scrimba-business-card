9d60b23f8675fcab358133c2e907510a9be45845
zkkv <zkkv@proton.me>
2024-06-26 21:29:33 +0200

    Add fonts and set up most colors except for links

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,18 @@ import Info from './Info'
 import About from './About'
 import Interests from './Interests'
 import Footer from './Footer'
+import personalImage from '../../assets/personal_image.png'
 
 export default function App() {
 	return (
-		<>
-			<Info />
-			<About />
-			<Interests />
+		<div id="top-level-container">
+			<img id="personal-image" src={personalImage} width="400px" />
+			<div id="text-container">
+				<Info />
+				<About />
+				<Interests />
+			</div>
 			<Footer />
-		</>
+		</div>
 	)
 }
