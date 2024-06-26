0b5cf226eb5c00bba74d288462768598bf98a27d
zkkv <zkkv@proton.me>
2024-06-26 22:58:30 +0200

    Configure email and linkedin links

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,9 +4,15 @@ export default function Info() {
 			<h1>Spider-Man</h1>
 			<h2>Superhero</h2>
 			<a id="personal-website" href="https://example.com">spiderman.com</a>
-			<menu>
-				<li><a href="mailto:user@example.com">Email</a></li>
-				<li><a href="https://www.linkedin.com">LinkedIn</a></li>
+			<menu id="main-links-menu">
+				<li><a id="email-anchor" href="mailto:user@example.com">
+					<img src="../../assets/email.svg" alt="Email pictogram" />
+					<div>Email</div>
+				</a></li>
+				<li><a id="linkedin-anchor" href="https://www.linkedin.com">
+					<img src="../../assets/linkedin.svg" alt="LinkedIn pictogram" />
+					<div>LinkedIn</div>
+				</a></li>
 			</menu>
 		</>
 	)
